975f5992ddb53e8494094caddaeca480a39d8b6d
taku10101 <taku1011ht@gmail.com>
2024-02-04 21:50:28 +0900

    add validate

--- cypress/e2e/email.cy.ts
+++ cypress/e2e/email.cy.ts
@@ -1,12 +1,17 @@
+/// <reference types="cypress" />
+
 describe("template spec", () => {
   it("passes", () => {
-    const email = "taku1011@dfd.com";
-    Cypress._.isString(email) && email.includes("@") && email.includes("."); //valid email
     cy.visit("http://localhost:5173");
 
+    const email = "taku1011htmail.com";
     cy.get('input[type="email"]').type(email);
-
     cy.get('input[type="password"]').type("password");
     cy.get('button[type="submit"]').click();
+
+    // 正規表現を使用してEメールが正しい形式であることを確認します。
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    //trueであればパス
+    assert.isTrue(emailRegex.test(email), "Email format is valid");
   });
 });
