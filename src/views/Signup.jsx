cd5dc035dd5e898406c1f0e134b9e57a1f576a52
taku10101 <taku1011ht@gmail.com>
2024-02-05 16:36:41 +0900

    form add

--- src/views/Signup.jsx
+++ src/views/Signup.jsx
@@ -1,7 +1,35 @@
 import React from "react";
+import FormProvider from "../components/FormProvider";
+import TextFieldComponent from "../components/TextFieldComponent";
 
 const Signup = () => {
-  return <div>Signup</div>;
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <FormProvider
+      defaultValues={{ email: "", password: "" }}
+      onSubmit={onSubmit}
+    >
+      <TextFieldComponent
+        name='email'
+        label='Email'
+        validationRules={{
+          required: "Email is required",
+          pattern: {
+            value: /^\S+@\S+$/i,
+            message: "Email is invalid",
+          },
+        }}
+      />
+      <TextFieldComponent
+        name='password'
+        label='Password'
+        validationRules={{ required: "Password is required", minLength: 6 }}
+      />
+    </FormProvider>
+  );
 };
 
 export default Signup;
