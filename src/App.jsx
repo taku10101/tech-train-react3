fe5fffc3a64cc84c328ffef0dc4ac7967dc93392
taku10101 <taku1011ht@gmail.com>
2024-02-05 12:51:30 +0900

    react-hook-form add

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Routes, BrowserRouter, Route } from "react-router-dom";
 import FormTest from "./ components/FormTest";
 import Login from "./ components/Login";
+import Signup from "./ components/Signup";
 const App = () => {
   return (
     <BrowserRouter>
@@ -9,6 +10,7 @@ const App = () => {
         <Route path='/' element={<h1>Welcome to the home page</h1>} />
         <Route path='/form-test' element={<FormTest />} />
         <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
       </Routes>
     </BrowserRouter>
   );
