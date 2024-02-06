75f989752a4a999177030de90d5a5d4d5dd7b667
taku10101 <taku1011ht@gmail.com>
2024-02-06 11:32:02 +0900

    api use addr

---
+++ src/api/user.js
@@ -0,0 +1,38 @@
+import { getToken } from "../utils/auth";
+const baseURL = "https://railway.bookreview.techtrain.dev";
+
+export const getUser = async () => {
+  const token = getToken();
+  const response = await fetch(`${baseURL}/users`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return response.json();
+};
+
+export const updateUser = async (name) => {
+  const token = getToken();
+  const response = await fetch(`${baseURL}/users`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({ name }),
+  });
+  return response.json();
+};
+
+export const uploadIcon = async (iconUrl) => {
+  const token = getToken();
+  const response = await fetch(`${baseURL}/icon`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "multipart/form-data",
+      Authorization: `Bearer ${token}`,
+    },
+    body: iconUrl,
+  });
+  return response.json();
+};
